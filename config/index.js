00c297a9ffec5475c0b57e7da4dac8a3386fd85c
Gwenael Pluchon <accounts@gwenp.fr>
2018-01-28 20:47:45 +0100

    update paths for dist/

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/blog-example-github-editor',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/blog-example-github-editor/dist',
 
     /**
      * Source Maps
